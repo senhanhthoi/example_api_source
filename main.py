8cca81d2f3c703c8ed6dc90d2058a2c5a80fb5c8
setnhanhthoi <senhanhthoi.nty@gmail.com>
2025-05-16 12:29:22 +0000

    ok

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route(\'/api/status\')
+@app.route('/api/status')
 def api_status():
     return jsonify({
         "status": "API is running",
         "timestamp": datetime.datetime.utcnow().isoformat() + "Z",
         "message": "Hello from the deployed API!",
-        "repository_name": os.getenv(\"GITHUB_REPOSITORY\", \"N/A\")
+        "repository_name": os.getenv("GITHUB_REPOSITORY", "N/A")
     })
 
-@app.route(\'/\')
+@app.route('/')
 def home():
-    return "API is alive! Visit /api/status for details.
+    return "API is alive! Visit /api/status for details."
 
-if __name__ == \"__main__\":
-    port = int(os.environ.get(\"PORT\", 8080))
-    app.run(debug=False, host=\"0.0.0.0\", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=False, host="0.0.0.0", port=port)
 
